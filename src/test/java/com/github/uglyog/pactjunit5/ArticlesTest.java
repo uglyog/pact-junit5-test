61cd52ee441dad2e74956b8db8f1660b258d1ea8
Ronald Holshausen <uglyog@gmail.com>
2019-06-01 15:14:06 +1000

    chore: added consumer pact test

---
+++ src/test/java/com/github/uglyog/pactjunit5/ArticlesTest.java
@@ -0,0 +1,83 @@
+package com.github.uglyog.pactjunit5;
+
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
+import au.com.dius.pact.consumer.junit5.PactTestFor;
+import au.com.dius.pact.model.RequestResponsePact;
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.fluent.Request;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(PactConsumerTestExt.class)
+@PactTestFor(providerName = "ArticlesProvider", port = "1234")
+public class ArticlesTest {
+  private Map<String, String> headers = MapUtils.putAll(new HashMap<>(), new String[] {
+    "Content-Type", "application/json"
+  });
+
+  @au.com.dius.pact.consumer.Pact(consumer = "ArticlesConsumer")
+  public RequestResponsePact articles(PactDslWithProvider builder) {
+    return builder
+      .given("Articles exist")
+      .uponReceiving("retrieving article data")
+        .path("/articles.json")
+        .method("GET")
+      .willRespondWith()
+        .headers(headers)
+        .status(200)
+        .body(
+          new PactDslJsonBody()
+            .minArrayLike("articles", 1)
+              .object("variants")
+                .eachKeyLike("0032")
+                  .stringType("description", "sample description")
+                  .closeObject()
+                .closeObject()
+              .closeObject()
+            .closeArray()
+        )
+      .toPact();
+  }
+
+  @au.com.dius.pact.consumer.Pact(consumer = "ArticlesConsumer")
+  public RequestResponsePact articlesDoNotExist(PactDslWithProvider builder) {
+    return builder
+      .given("No articles exist")
+      .uponReceiving("retrieving article data")
+      .path("/articles.json")
+      .method("GET")
+      .willRespondWith()
+      .headers(headers)
+      .status(404)
+      .toPact();
+  }
+
+  @Test
+  @PactTestFor(pactMethod = "articles")
+  void testArticles(au.com.dius.pact.consumer.MockServer mockServer) throws IOException {
+    HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/articles.json").execute().returnResponse();
+    assertThat(httpResponse.getStatusLine().getStatusCode(), is(equalTo(200)));
+    assertThat(IOUtils.toString(httpResponse.getEntity().getContent()),
+      is(equalTo("{\"articles\":[{\"variants\":{\"0032\":{\"description\":\"sample description\"}}}]}")));
+  }
+
+  @Test
+  @PactTestFor(pactMethod = "articlesDoNotExist")
+  void testArticlesDoNotExist(au.com.dius.pact.consumer.MockServer mockServer) throws IOException {
+    HttpResponse httpResponse = Request.Get(mockServer.getUrl() + "/articles.json").execute().returnResponse();
+    assertThat(httpResponse.getStatusLine().getStatusCode(), is(equalTo(404)));
+    assertThat(IOUtils.toString(httpResponse.getEntity().getContent()), is(equalTo("")));
+  }
+}
